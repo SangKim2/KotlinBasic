b3322db1937be468f35bbc34632fbbb35d721ce8
Kim Sang Hyeong <tkdgud0123@naver.com>
2023-01-03 01:12:33 +0900

    git interlock test sss

--- app/src/main/java/com/example/inflearnbasic/MainActivity.kt
+++ app/src/main/java/com/example/inflearnbasic/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // practice book ss
+        // practice book ss ffff
     }
 }
